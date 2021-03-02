be9921fda3bff7456fcde2a8ac32b65c2835d54d
Smack <kovacsmyles1@gmail.com>
2021-03-01 21:25:48 -0600

    completed application MVP

--- Develop/server.js
+++ Develop/server.js
@@ -5,6 +5,7 @@ const PORT = 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 const { notes } = require('./db/db');
 
@@ -18,6 +19,20 @@ function createNewNote(body, notesArray) {
 return note;
 }
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id) [0];
+    return result;
+}
+
+// function findByIdAndRemove(id, notesArray) {
+//     const result = notesArray.filter(note => note.id === id);   
+    
+//     return result != id;
+    
+//      result.splice(id);
+//      return result;
+// }
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
@@ -27,9 +42,30 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-    return res.json(notes);
+    res.json(notes);
 })
 
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+        //res.status(200).json({msg: "get id route"})
+    } else {
+        //res.status(400).json({msg: "get id route error"})
+        res.send(404);
+    }
+});
+
+// app.delete('/api/notes/:id', (req, res) => {
+//     const result = findByIdAndRemove(req.params.id, notes); 
+//     if (result) {
+//         res.json(result);
+//         //res.status(200).json({msg: "get id route"})
+//     } else {
+//         //res.status(400).json({msg: "get id route error"})
+//         res.send(404);
+//     }
+// });
 
 app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
